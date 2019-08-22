4852d8c0a90bd0d94eb8591cf5369aee4a5b571e
dss13 <s.sumugan007@gmail.com>
2019-08-22 17:48:29 +0530

    add orderbook

--- src/main/java/com/ordermatching/demo/OrderBook.java
+++ src/main/java/com/ordermatching/demo/OrderBook.java
@@ -1,5 +1,37 @@
 package com.ordermatching.demo;
 
-public class OrderBook {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "\"OrderBook\"")
+public class OrderBook {
+	@Id
+	String OrderNumber;
+	String SecurityCode;
+	Date TradeTime;
+	Float Price;
+	String Direction;
+	int Quantity;
+	String OrderStatus;
+	String PriceCondition;
+	String CustomerCode;
+	
+	public OrderBook(String securityCode, Date tradeTime, Float price, String direction,
+			int quantity, String priceCondition, String customerCode) {
+		super();
+		Date d = new Date();
+		OrderNumber = d.toString() + Math.random();
+		SecurityCode = securityCode;
+		TradeTime = new Date();
+		Price = price;
+		Direction = direction;
+		Quantity = quantity;
+		OrderStatus = "Pending";
+		PriceCondition = priceCondition;
+		CustomerCode = customerCode;
+	}
 }
