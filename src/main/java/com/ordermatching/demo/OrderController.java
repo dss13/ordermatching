8cbd07b67e9e9369fc9617242544eb1e7cf8f225
PujaLuciX <puja28dreamiit@gmail.com>
2019-08-23 12:19:52 +0530

    Place order functionality implemented

--- src/main/java/com/ordermatching/demo/OrderController.java
+++ src/main/java/com/ordermatching/demo/OrderController.java
@@ -1,9 +1,12 @@
 package com.ordermatching.demo;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,19 +18,65 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ordermatching.demo.models.OrderBook;
+import com.ordermatching.demo.models.SecuritiesMaster;
+import com.ordermatching.demo.repositories.CustomerRepository;
 import com.ordermatching.demo.repositories.OrderBookRepository;
+import com.ordermatching.demo.repositories.SecuritiesMasterRepository;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 	@Autowired
 	OrderBookRepository repo;
+	@Autowired
+	CustomerRepository custRepo;
+	@Autowired
+	SecuritiesMasterRepository securityRepo;
+	
 	@CrossOrigin(origins = "http://localhost:8000")
 	@PostMapping("/add")
 	public Map<String, String> newOrder(@RequestBody OrderBook orderbook) {
 		HashMap<String, String> map = new HashMap<>();
 		orderbook.setTradeTime(new Date());
-		orderbook.setOrderNumber(new Date()+""+Math.random());
+		
+		String timestamp = new SimpleDateFormat("yyMMddHHmmss").format(Calendar.getInstance().getTime());
+		Random rand= new Random();
+		String random = String.format("%04d",rand.nextInt(10000));
+		orderbook.setOrderNumber(timestamp + random);
+		
+		/* Order Status updated */
+		String SecurityCode = orderbook.getSecurityCode();
+		SecuritiesMaster requiredSecurity = null;
+		List<SecuritiesMaster> tempSecurityList = (List<SecuritiesMaster>)securityRepo.findAll();
+		for(int i=0;i<tempSecurityList.size();i++) {
+			if(tempSecurityList.get(i).getSecurityCode().equals(SecurityCode))
+				requiredSecurity = tempSecurityList.get(i);
+		}
+		if(requiredSecurity == null) {
+			map.put("code", "0");
+			return map;
+		}
+		float SP = requiredSecurity.getSellingPrice();
+		float BP = requiredSecurity.getBuyingPrice();		
+		float price = orderbook.getPrice();
+		String status = orderbook.getOrderStatus();
+		if(orderbook.getDirection().equals("Buy") && status.equals("Pending"))
+		{
+			if(price < 0.9*BP || price > 1.1*BP)
+			{
+				status = "Rejected";
+			}
+		}
+		else if(orderbook.getDirection().equals("Sell") && status.equals("Pending"))
+		{
+			if(price < 0.9*SP || price > 1.1*SP)
+			{
+				status = "Rejected";
+			}
+		}
+		
+		orderbook.setOrderStatus(status);
+		
 		try {
 			
 			repo.save(orderbook);
